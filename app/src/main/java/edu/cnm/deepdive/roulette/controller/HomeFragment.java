aaf76efa480d3c2a4a982bb0cac09e6275a2d94e
Kazpenn <kazpenn2020@gmail.com>
2021-02-15 16:01:11 -0700

    implemented animation.

--- app/src/main/java/edu/cnm/deepdive/roulette/controller/HomeFragment.java
+++ app/src/main/java/edu/cnm/deepdive/roulette/controller/HomeFragment.java
@@ -5,20 +5,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.RotateAnimation;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import edu.cnm.deepdive.roulette.R;
 import edu.cnm.deepdive.roulette.databinding.FragmentHomeBinding;
 import edu.cnm.deepdive.roulette.viewmodel.HomeViewModel;
+import java.security.SecureRandom;
+import java.util.Random;
 
 public class HomeFragment extends Fragment {
 
   private HomeViewModel homeViewModel;
   private FragmentHomeBinding binding;
+  private boolean spinning;
+  private Random rng;
+
+  @Override
+  public void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    rng = new SecureRandom();
+  }
 
   public View onCreateView(@NonNull LayoutInflater inflater,
       ViewGroup container, Bundle savedInstanceState) {
@@ -26,6 +37,11 @@ public class HomeFragment extends Fragment {
     binding.spinWheel.setOnClickListener(new OnClickListener() {
       @Override
       public void onClick(View v) {
+        if(!spinning) {
+          spinning = true;
+          binding.spinWheel.setEnabled(false);
+          binding.rouletteValue.setVisibility(View.INVISIBLE);
+        }
         homeViewModel.spinWheel();
       }
     });
@@ -42,5 +58,48 @@ public class HomeFragment extends Fragment {
         binding.rouletteValue.setText(s);
       }
     });
+    homeViewModel.getPocketIndex().observe(getViewLifecycleOwner(), new Observer<Integer>() {
+      @Override
+      public void onChanged(Integer pocketIndex) {
+        float centerX = binding.rouletteWheel.getWidth() / 2f;
+        float centerY = binding.rouletteWheel.getHeight() / 2f;
+        float currentRotation = binding.rouletteWheel.getRotation();
+        float finalRotation = - 360 * pocketIndex / 38f;
+        binding.rouletteWheel.setPivotX(centerX);
+        binding.rouletteWheel.setPivotY(centerY);
+        RotateAnimation rotation = new RotateAnimation(
+            0, (finalRotation - currentRotation) - 360 * (3 + rng.nextInt(3)), centerX, centerY);
+        rotation.setDuration(2000 + rng.nextInt(3000));
+        rotation.setAnimationListener(new AnimationFinalizer(finalRotation));
+        binding.rouletteWheel.startAnimation(rotation);
+      }
+    });
+  }
+
+  private class AnimationFinalizer implements AnimationListener {
+
+    private final float finalRotation;
+
+    private AnimationFinalizer(float finalRotation) {
+      this.finalRotation = finalRotation;
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+      binding.rouletteWheel.setRotation(finalRotation);
+      spinning = false;
+      binding.spinWheel.setEnabled(true);
+      binding.rouletteValue.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
+    }
   }
 }
