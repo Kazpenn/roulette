a9b4e8ce096a660dc18eaa4bf73f38b238e60b60
Kazpenn <kazpenn2020@gmail.com>
2021-02-23 14:50:44 -0700

    Saving spins with spin repository.

--- app/src/main/java/edu/cnm/deepdive/roulette/controller/HomeFragment.java
+++ app/src/main/java/edu/cnm/deepdive/roulette/controller/HomeFragment.java
@@ -3,7 +3,6 @@ package edu.cnm.deepdive.roulette.controller;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
@@ -11,8 +10,9 @@ import android.view.animation.RotateAnimation;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 import edu.cnm.deepdive.roulette.databinding.FragmentHomeBinding;
 import edu.cnm.deepdive.roulette.viewmodel.HomeViewModel;
 import java.security.SecureRandom;
@@ -45,7 +45,7 @@ public class HomeFragment extends Fragment {
   }
 
   private void spinWheel() {
-    if(!spinning) {
+    if (!spinning) {
       spinning = true;
       binding.spinWheel.setEnabled(false);
       binding.rouletteValue.setVisibility(View.INVISIBLE);
@@ -57,21 +57,31 @@ public class HomeFragment extends Fragment {
   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
     homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+    getLifecycle().addObserver(homeViewModel);
     homeViewModel.getRouletteValue().observe(getViewLifecycleOwner(),
         (s) -> binding.rouletteValue.setText(s));
     homeViewModel.getPocketIndex().observe(getViewLifecycleOwner(), this::startAnimation);
+    homeViewModel.getThrowable().observe(getViewLifecycleOwner(), (throwable) -> {
+      if (throwable != null) {
+        //noinspection ConstantConditions
+        Snackbar.make(getContext(), binding.getRoot(), throwable.getMessage(),
+            BaseTransientBottomBar.LENGTH_INDEFINITE).show();
+      }
+    });
   }
 
   private void startAnimation(Integer pocketIndex) {
     float centerX = binding.rouletteWheel.getWidth() / 2f;
     float centerY = binding.rouletteWheel.getHeight() / 2f;
     float currentRotation = binding.rouletteWheel.getRotation();
-    float finalRotation = -DEGREES_PER_REVOLUTION * pocketIndex / (float) HomeViewModel.POCKETS_ON_WHEEL;
+    float finalRotation =
+        -DEGREES_PER_REVOLUTION * pocketIndex / (float) HomeViewModel.POCKETS_ON_WHEEL;
     binding.rouletteWheel.setPivotX(centerX);
     binding.rouletteWheel.setPivotY(centerY);
     RotateAnimation rotation = new RotateAnimation(
-        0, (finalRotation - currentRotation) - DEGREES_PER_REVOLUTION * (MIN_FULL_ROTATIONS + rng.nextInt(
-        MAX_FULL_ROTATIONS - MIN_FULL_ROTATIONS + 1)), centerX, centerY);
+        0, (finalRotation - currentRotation) - DEGREES_PER_REVOLUTION * (MIN_FULL_ROTATIONS + rng
+        .nextInt(
+            MAX_FULL_ROTATIONS - MIN_FULL_ROTATIONS + 1)), centerX, centerY);
     rotation.setDuration(MIN_ROTATION_TIME + rng.nextInt(MAX_ROTATION_TIME));
     rotation.setAnimationListener(new AnimationFinalizer(finalRotation));
     binding.rouletteWheel.startAnimation(rotation);
@@ -97,6 +107,7 @@ public class HomeFragment extends Fragment {
       binding.spinWheel.setEnabled(true);
       binding.rouletteValue.setVisibility(View.VISIBLE);
     }
+
     @Override
     public void onAnimationRepeat(Animation animation) {
 
